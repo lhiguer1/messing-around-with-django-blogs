aa37d2336eebecf353a3c94ae827c244407df0b6
Leonel Higuera <lhiguer1@asu.edu>
2022-04-11 01:01:21 -0700

    change settings to not recreate SECRET_KEY if it already exists

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -21,9 +21,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = get_random_secret_key()
-with open('secret_key.txt', 'w') as f:
-    f.write(f'{SECRET_KEY=}')
+try:
+    with open('secret_key.txt') as f:
+        SECRET_KEY = f.read().strip()
+except FileNotFoundError:
+    SECRET_KEY = get_random_secret_key()
+    with open('secret_key.txt', 'w') as f:
+        f.write(SECRET_KEY)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
